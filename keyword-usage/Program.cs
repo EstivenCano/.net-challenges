f1b1555aaf0190d19829cc00c679577da8df8034
Estiven Cano <estiven.canou@perficient.com>
2024-06-04 20:10:39 -0500

    Add project files.

---
+++ keyword-usage/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+User newUser = new User("Estiven", 25, "test@email.com");
+
+newUser.logUser();
---
+++ keyword-usage/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+public class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+
+    public User(string name, int age, string email)
+    {
+        Name = name;
+        Age = age;
+        Email = email;
+    }
+
+    public void logUser()
+    {
+        Type userType = typeof(User);        
+
+        // Get the properties of the User class
+        PropertyInfo[] properties = userType.GetProperties();
+
+        // Print each property name and type
+        foreach (PropertyInfo property in properties)
+        {
+            Console.WriteLine($"{property.Name}: {property.GetValue(this)}");
+        }        
+    }
+}
