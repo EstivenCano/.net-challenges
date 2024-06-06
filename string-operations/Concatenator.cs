4eb408d7966b70cc2ca5016ec56f3e78ef957a38
Estiven Cano <estiven.canou@perficient.com>
2024-06-06 13:09:06 -0500

    [Add] String operations benchmark

---
+++ string-operations/Concatenator.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+public class Concatenator
+{
+    [Benchmark]
+    [Arguments("Celeste", "Chase", "Korah", "Dalila")]    
+    public string WithLiterals(string name1, string name2, string name3, string name4)
+    {
+        return "Hello " + name1 + " " + name2 + " " + name3 + " " + name4;
+    }
+
+    [Benchmark]
+    [Arguments("Celeste", "Chase", "Korah", "Dalila")]
+    public string WithInterpolation(string name1, string name2, string name3, string name4)
+    {
+        return $"Hello {name1} {name2} {name3} {name4}";
+    }
+
+    [Benchmark]
+    [Arguments("Celeste", "Chase", "Korah", "Dalila")]
+    public string WithConcat(string name1, string name2, string name3, string name4)
+    {  
+        return string.Concat("Hello ", name1," ", name2," ", name3," ", name4);
+    }
+
+    [Benchmark]
+    [Arguments("Celeste", "Chase", "Korah", "Dalila")]
+    public string WithJoin(string name1, string name2, string name3, string name4)
+    {
+        string[] names = ["Hello", name1, name2, name3, name4];
+        return string.Join(" ", names);
+    }
+
+    [Benchmark]
+    [Arguments("Celeste", "Chase", "Korah", "Dalila")]
+    public string WithLINQ(string name1, string name2, string name3, string name4)
+    {
+        string[] names = ["Hello", name1, name2, name3, name4];
+        return names.Aggregate((partialPhrase, word) => $"{partialPhrase} {word}");
+
+    }
+}
+
---
+++ string-operations/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+// Exercise 01
+
+Console.WriteLine("Exercise 01:");
+//string[] names = ["one", "two", "three"];
+
+//string literalsResult = Concatenator.WithLiterals("Celeste", "Chase", "Korah", "Dalila");
+//string interpolationResult = Concatenator.WithInterpolation("Celeste", "Chase", "Korah", "Dalila");
+//string concatResult = Concatenator.WithConcat("Celeste", "Chase", "Korah", "Dalila");
+//string joinResult = Concatenator.WithJoin("Celeste", "Chase", "Korah", "Dalila");
+//string lineQResult = Concatenator.WithLINQ("Celeste", "Chase", "Korah", "Dalila");
+
+//Console.WriteLine(literalsResult);
+//Console.WriteLine(interpolationResult);
+//Console.WriteLine(concatResult);
+//Console.WriteLine(joinResult);
+//Console.WriteLine(lineQResult);
+
+var summary = BenchmarkRunner.Run<Concatenator>();
+
+/* --------------------------------------------------------- */
